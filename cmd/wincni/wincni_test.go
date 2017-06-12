9fbf05b8f3f31163f8a45ce267f7cd42a76b2a75
Sam Smith <sesmith177@gmail.com>
2017-06-12 12:28:25 -0400

    Fill more config in for add / del

--- cmd/wincni/wincni_test.go
+++ cmd/wincni/wincni_test.go
@@ -14,25 +14,50 @@ var _ = Describe("wincni", func() {
 		networkConfig *libcni.NetworkConfig
 		runtimeConf   *libcni.RuntimeConf
 		err           error
+		confJSON      string
 	)
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		CNIConfig = &libcni.CNIConfig{Path: []string{filepath.Dir(wincniBin)}}
-		runtimeConf = &libcni.RuntimeConf{}
+		runtimeConf = &libcni.RuntimeConf{
+			ContainerID: "handle123",
+			NetNS:       "path_to_namespace",
+			IfName:      "eth123",
+		}
 
-		confJSON := []byte(`{"cniVersion": "0.3.1", "type": "wincni"}`)
-		networkConfig, err = libcni.ConfFromBytes(confJSON)
+		networkConfig, err = libcni.ConfFromBytes([]byte(confJSON))
 		Expect(err).To(BeNil())
 	})
 
 	Describe("add", func() {
+		BeforeEach(func() {
+			confJSON = `
+{
+	"cniVersion": "0.3.1",
+	"type": "wincni",
+	"name": "container_net",
+	"runtimeConfig": {
+		"portMappings": [ { "container_port": 8080, "host_port": 1234 } ]
+	}
+}`
+		})
+
 		It("returns successfully", func() {
-			_, err := CNIConfig.AddNetwork(networkConfig, runtimeConf)
+			result, err := CNIConfig.AddNetwork(networkConfig, runtimeConf)
 			Expect(err).To(BeNil())
+			Expect(result).NotTo(BeNil())
 		})
 	})
 
 	Describe("delete", func() {
+		BeforeEach(func() {
+			confJSON = `
+{
+	"cniVersion": "0.3.1",
+	"type": "wincni",
+	"name": "container_net"
+}`
+		})
 		It("returns successfully", func() {
 			Expect(CNIConfig.DelNetwork(networkConfig, runtimeConf)).To(Succeed())
 		})
